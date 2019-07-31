dac9ae8ca874f8b3bf1b9337cb6c9707db8e08ce
Mikhail Gareev <grvmkhl@gmail.com>
2019-07-31 21:28:06 +0300

    add facet plots

--- shinyapp.R
+++ shinyapp.R
@@ -1,8 +1,24 @@
 rm(list=ls())
-trainout <- c(out1, out2)
+trainout <- c(out1, out2, out3)
+
+out1 %>% 
+  walk(function(x){
+    if(x$model == 'lasso' &
+       x$h == 3 &
+       x$lag == 3){
+      print(x$startdt)
+      x$model_fit %>% predict(type='coefficients') %>% print
+      readline()
+    } else {
+      NULL
+    }
+
+    
+  })
 
 
-train2 <- trainout %>% 
+
+out_short <- trainout %>% 
   map_dfr(function(x){
       data.frame(model=x$model,
                  lag = x$lag,
@@ -14,7 +30,9 @@ train2 <- trainout %>%
     
   })
 
-
+# save(out_short, file='data/out_short.RData')
+load('data/out_short.RData')
+load('data/raw.RData')
 ytrue <- rawdata$investment
 
 
@@ -24,29 +42,29 @@ ui <- pageWithSidebar(
   headerPanel('Прогнозирование инвестиций'),
   sidebarPanel(
     selectizeInput('startdt', 'Start date',
-                   train2$startdt %>% unique),
+                   out_short$startdt %>% unique),
     
     numericInput('lag',
                  'Lag',
-                 value = train2$lag %>% min,
-                 min = train2$lag %>% min, 
-                 max = train2$lag %>% max),
+                 value = out_short$lag %>% max,
+                 min = out_short$lag %>% min, 
+                 max = out_short$lag %>% max),
     numericInput('h',
                  'Horizon',
-                 value =  train2$h %>% max,
-                 min = train2$h %>% min, 
-                 max = train2$h %>% max),
+                 value =  out_short$h %>% median,
+                 min = out_short$h %>% min, 
+                 max = out_short$h %>% max),
     selectizeInput('model', 'Model',
-                   train2$model %>% unique),
+                   out_short$model %>% unique),
     actionButton("update", "Update View")
     ),
-  mainPanel(plotOutput('mainplot'))
+  mainPanel(plotOutput('forecast'))
   )
 
 server <- function(input, output){
 
   train3 <- eventReactive(input$update,{
-  train2 %>%
+  out_short %>%
     filter(model == input$model,
            lag == input$lag,
            startdt == input$startdt,
@@ -55,39 +73,89 @@ server <- function(input, output){
   })
 
   
-  output$mainplot <- renderPlot({
-
+  output$forecast <- renderPlot({
+    
     ggplot()+
       geom_line(data = train3(),
                 aes(x = date,
-                    y = pred,
-                    col= 'pred')) +
+                    y = pred),
+                    col= 'red') +
       geom_line(data = NULL,
                 aes(x = ytrue %>%
                       time %>%
                       as.Date,
                     y = ytrue %>%
                       as.numeric %>%
-                      diff.xts(lag = 4, log=TRUE), color = 'true'))+
+                      diff.xts(lag = 4, log=TRUE)), color = 'black')+
       scale_x_date(date_breaks = "5 year", 
                    labels=date_format("%Y"),
                    limits = as.Date(c(train3()$date %>% min,
-                                      train3()$date %>% max)))
+                                      train3()$date %>% max)))+
+      geom_vline(aes(xintercept  = as.Date(train3()$enddt %>% min)),
+                 color = "red",linetype="dashed")+
+      labs(title = "",
+           y = "Изменение инвестиций (log)",
+           x = "Дата")
+      
+    
+    
   })
+  
+  
 }
 
 
 shinyApp(ui, server)
-# start date
-# end date
-# lag
-# h
-# model
-# features
-# conf int (true or false)
+
+
 # server ----
 # main plot
 # metrics (rmse, r2, mae, mape)
 # knowcasting
 
+out_true <- data.frame(date = ytrue %>%
+             time %>%
+             as.Date,
+           true = ytrue %>%
+             as.numeric %>%
+             diff.xts(lag = 4, log=TRUE),
+           true_lag = ytrue %>%
+             as.numeric %>%
+             lag.xts(lag = 4)) %>%
+  inner_join(out_short %>%
+               group_by(model, lag, startdt, enddt, h) %>%
+               mutate(pred = lag(pred, min(h))), by = 'date')
 
+for(modeli in out_true$model %>% unique){
+  print(modeli)
+  plot <- out_true %>%
+    filter(model == modeli,
+           startdt == max(startdt),
+           lag != 0,
+           h != 0) %>%
+    ggplot()+
+    geom_line(aes(x = date, y = pred), color = 'red')+
+    geom_line(aes(x = date, y = true))+
+    geom_vline(aes(xintercept  = min(enddt)),
+               color = "red",linetype="dashed")+
+    labs(title = modeli) +
+    facet_grid(vars(lag), vars(h))
+  png(paste0('plot/facets/',modeli,".png"), width = 1000, height = 700)
+  print(plot)
+  dev.off()
+}
+
+out_true %>%
+  filter(
+         startdt == max(startdt),
+         lag != 0,
+         h != 0) %>%
+  ggplot()+
+  geom_line(aes(x = date, y = pred, color = model))+
+  geom_line(aes(x = date, y = true), color = 'black')+
+  geom_vline(aes(xintercept  = min(enddt)),
+             color = "red",linetype="dashed")+
+  labs(title = modeli) +
+  facet_grid(vars(lag), vars(h))
+
+  
