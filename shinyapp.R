c88bc4bc414d1ba31ea6b4287432bbe189a0a2bc
Mikhail Gareev <grvmkhl@gmail.com>
2019-09-05 19:54:48 +0300

    Update shinyapp.R

--- shinyapp.R
+++ shinyapp.R
@@ -12,8 +12,25 @@ out_short <-
                  date = x$date,
                  pred=x$pred) 
     
-  })
+  }) %>%
+  filter(enddt <= as.Date('2015-01-01'))
 
+
+
+# Vectorized SWITCH:
+correct.names <- Vectorize(vectorize.args = "model",
+                 FUN = function(model) {
+                   switch(as.character(model),
+                          'lasso' = 'LASSO',
+                          'postlasso' = 'Post-LASSO',
+                          'adalasso' = 'Adaptive LASSO',
+                          'ridge' = 'Ridge',
+                          'rf' = 'Random Forest',
+                          'ss' = 'Spike-and-Slab',
+                          'arima' = 'ARIMA',
+                          'rw' = 'Random Walk')})
+
+out_short$model <- correct.names(model = out_short$model)
 
 # save(out_short, file='data/out_short.RData')
 rm(list=ls())
@@ -59,7 +76,7 @@ scoredf_raw <- get.score(out_true %>%
 
 # техническая таблица
 benchmarkscore <- scoredf_raw %>%
-  filter(model == 'rw')
+  filter(model == 'Random Walk')
 
 # таблица scoredf (rmse даны относительно rw)
 scoredf <- get.score(out_true %>%
@@ -105,8 +122,13 @@ ui <- pageWithSidebar(
     selectizeInput('startdt', 'Выберите левую границу тренировочной выборки',
                    out_short$startdt %>% unique %>% set_names(as.yearqtr(.)) ,
                    selected = out_short$startdt %>% unique %>% last),
-    selectizeInput('enddt', 'Выберите правую границу тренировочной выборки',
-                   out_short$enddt %>% unique %>% set_names(as.yearqtr(.)) ),
+    # selectizeInput('enddt', 'Выберите правую границу тренировочной выборки',
+    #                out_short$enddt %>% unique %>% set_names(as.yearqtr(.))),
+    sliderInput('enddt',  'Выберите правую границу тренировочной выборки',
+                min = out_short$enddt %>% unique %>% min,
+                max = out_short$enddt %>% unique %>% max,
+                step = 92,value =out_short$enddt %>% unique %>% min,animate = TRUE
+                    ),
     
     numericInput('lag',
                  'Выберите количество лагов в модели (кварталов)',
@@ -139,7 +161,8 @@ ui <- pageWithSidebar(
     hr(),
     
     
-    actionButton("update", "Произвести расчёты")
+    actionButton("update", "Произвести расчёты"),
+    actionButton("animate", "animate")
     ),
   mainPanel(column(12,
     plotOutput('forecast'),
@@ -163,64 +186,125 @@ server <- function(input, output){
     }
     
   })
+  
 
-  df.forecast <- eventReactive(input$update,{
-    
-    out <- out_short %>%
-      filter(model %in% input$model,
-             
-             startdt == input$startdt,
-             enddt == input$enddt, 
-             h == input$h)
-    
-    
-    if(input$optlag){
-      optlag <- scoredf %>% 
-        filter(type == 'train',
-               model %in% input$model,
-               h == input$h,
+
+  # df.forecast <- eventReactive(input$update,{
+  #   
+  #   out <- out_short %>%
+  #     filter(model %in% input$model,
+  #            
+  #            startdt == input$startdt,
+  #            enddt == input$enddt, 
+  #            h == input$h)
+  #   
+  #   
+  #   if(input$optlag){
+  #     optlag <- scoredf %>% 
+  #       filter(type == 'train',
+  #              model %in% input$model,
+  #              h == input$h,
+  #              startdt == input$startdt,
+  #              enddt == input$enddt
+  #       ) %>%
+  #       group_by(model) %>%
+  #       filter(rmse == min(rmse)) %>%
+  #       filter(lag == min(lag)) %>%
+  #       ungroup %>%
+  #       select(model, lag) %>%
+  #       unique
+  #     
+  #     
+  #     out %<>%
+  #       split(.$model) %>%
+  #       map_dfr(function(x){
+  #         x %>%
+  #           filter(lag == optlag$lag[which(optlag$model ==
+  #                                            (x$model %>% first))])
+  #       })
+  #     
+  #     
+  #     
+  #   } else {
+  #     
+  #     out  %<>%
+  #       filter(lag == input$lag)
+  #       
+  #   }
+  #   if(nrow(out) != 0){
+  #   
+  #   out %>%
+  #     mutate(pred = pred %>% lag(n = input$h)) %>%
+  #     filter(date >= c(ifelse(input$onlytrain,
+  #                             (enddt %>% as.numeric() + 100) %>%
+  #                               as.Date() %>%
+  #                               as.yearqtr() %>%
+  #                               as.Date,
+  #                             date %>% min))) %>%
+  #     na.omit
+  #   } else {
+  #       data.frame()
+  #     }
+  # })
+  
+  
+  df.forecast <- function(){
+
+      out <- out_short %>%
+        filter(model %in% input$model,
+
                startdt == input$startdt,
-               enddt == input$enddt
-        ) %>%
-        group_by(model) %>%
-        filter(rmse == min(rmse)) %>%
-        filter(lag == min(lag)) %>%
-        ungroup %>%
-        select(model, lag) %>%
-        unique
-      
-      
-      out %<>%
-        split(.$model) %>%
-        map_dfr(function(x){
-          x %>%
-            filter(lag == optlag$lag[which(optlag$model ==
-                                             (x$model %>% first))])
-        })
-      
-      
-      
-    } else {
-      
-      out  %<>%
-        filter(lag == input$lag)
-        
-    }
-    if(nrow(out) != 0){
-    
-    out %>%
-      mutate(pred = pred %>% lag(n = input$h)) %>%
-      filter(date >= c(ifelse(input$onlytrain,
-                              (enddt %>% as.numeric() + 100) %>%
-                                as.Date() %>%
-                                as.yearqtr() %>%
-                                as.Date,
-                              date %>% min))) %>%
-      na.omit
-    } else {
-        data.frame()
+               enddt == input$enddt %>% as.yearqtr %>% as.Date,
+               h == input$h)
+
+
+      if(input$optlag){
+        optlag <- scoredf %>%
+          filter(type == 'train',
+                 model %in% input$model,
+                 h == input$h,
+                 startdt == input$startdt,
+                 enddt == input$enddt %>% as.yearqtr %>% as.Date
+          ) %>%
+          group_by(model) %>%
+          filter(rmse == min(rmse)) %>%
+          filter(lag == min(lag)) %>%
+          ungroup %>%
+          select(model, lag) %>%
+          unique
+
+
+        out %<>%
+          split(.$model) %>%
+          map_dfr(function(x){
+            x %>%
+              filter(lag == optlag$lag[which(optlag$model ==
+                                               (x$model %>% first))])
+          })
+
+
+
+      } else {
+
+        out  %<>%
+          filter(lag == input$lag)
+
       }
-  })
+      if(nrow(out) != 0){
+
+      out %>%
+        mutate(pred = pred %>% lag(n = input$h)) %>%
+        filter(date >= c(ifelse(input$onlytrain,
+                                (enddt %>% as.numeric() + 100) %>%
+                                  as.Date() %>%
+                                  as.yearqtr() %>%
+                                  as.Date,
+                                date %>% min))) %>%
+        na.omit
+      } else {
+          data.frame()
+        }
+    }
   
   datebreaks <- eventReactive(input$update,{
       ifelse(input$onlytrain,
@@ -237,8 +321,7 @@ server <- function(input, output){
   )
   
   limits <- eventReactive(input$update,{
-    
-    print(df.forecast()$date %>% max)
+
     
     x <- c(df.forecast()$date %>% min,
            df.forecast()$date %>% max)
@@ -260,19 +343,70 @@ server <- function(input, output){
   
   output$forecast <- renderPlot({
     if(df.forecast() %>% nrow != 0){
-      ggplot()+
+      
+      
+      g_legend<-function(a.gplot){
+        tmp <- ggplot_gtable(ggplot_build(a.gplot))
+        leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+        legend <- tmp$grobs[[leg]]
+        return(legend)}
+      
+      p1 <- ggplot() + geom_line(data = NULL,
+                                 aes(x = ytrue %>%
+                                       time %>%
+                                       as.Date,
+                                     y = ytrue %>%
+                                       as.numeric %>%
+                                       diff.xts(lag = 4, log=TRUE),
+                                     color='Исходный ряд',
+                                 linetype = 'Исходный ряд',
+                                 size = 'Исходный ряд'))+
+        
+        scale_color_manual(values = 'black')+
+        scale_linetype_manual(values = 'dashed')+
+        scale_size_manual(values = 1)+
+        guides(colour = guide_legend(""),
+               size = guide_legend(""),
+               linetype = guide_legend(""))+
+        theme(legend.position="right",
+              legend.justification="left",
+              legend.margin=ggplot2::margin(0,0,0,0),
+              legend.box.margin=ggplot2::margin(10,10,10,10))
+      
+      p2 <- ggplot() +
         geom_line(data = df.forecast(),
-                  aes(x = date,
-                      y = pred,
-                      color = model), size = 1) +
+                         aes(x = date,
+                             y = pred,
+                             color = model), size = 1) +
+        scale_color_discrete(name = "Модель")+
+        theme(legend.position="right",
+              legend.justification="left",
+              legend.margin=ggplot2::margin(0,0,0,0),
+              legend.box.margin=ggplot2::margin(10,10,10,10))
+        
+      
+      
+      
+      
+      
+      p <- ggplot() +
+        geom_line(data = df.forecast(),
+                               aes(x = date,
+                                   y = pred,
+                                   color = model), size = 1) +
         geom_line(data = NULL,
-                  aes(x = ytrue %>%
-                        time %>%
-                        as.Date,
-                      y = ytrue %>%
-                        as.numeric %>%
-                        diff.xts(lag = 4, log=TRUE)), 
-                  color = 'black',size = 1, linetype="dashed")+
+                    aes(x = ytrue %>%
+                          time %>%
+                          as.Date,
+                        y = ytrue %>%
+                          as.numeric %>%
+                          diff.xts(lag = 4, log=TRUE)
+                        ),
+                  color='black',
+                    size = 1, linetype="dashed")+
+        scale_color_discrete(guide="none")+
+        
+        
         scale_x_date(date_breaks = datebreaks(),
                      labels=date_format("%Y"),
                      limits = limits()$x)+
@@ -281,9 +415,23 @@ server <- function(input, output){
 
         geom_vline(aes(xintercept  = as.Date(df.forecast()$enddt %>% min)),
                    color = "red",linetype="dashed", alpha = vlinealpha())+
+        geom_rect(aes(xmin=(df.forecast()$enddt %>% min + 100) %>% as.yearqtr %>% as.Date,
+                  xmax=(df.forecast()$enddt %>% min +366*3) %>% as.yearqtr %>% as.Date,
+                  ymin=-Inf, ymax=Inf),
+                  fill="black", alpha=0.2)
+      
         labs(title = "",
              y = "Изменение инвестиций (log)",
              x = "Дата")
+      
+      
+
+
+      grid.arrange(p,
+                   arrangeGrob(g_legend(p2),g_legend(p1), nrow=2),
+                               ncol=2,widths=c(7,1))
+
+      
     }
     
     
@@ -291,6 +439,8 @@ server <- function(input, output){
     
     
   })
+
+  
   
   df.score <- eventReactive(input$update,{
     
@@ -303,7 +453,7 @@ server <- function(input, output){
                   lag == input$lag,
              h == input$h,
                   startdt == input$startdt,
-             enddt == input$enddt
+             enddt == input$enddt %>% as.yearqtr %>% as.Date
                   ) %>%
       select(model, rmse)
     
@@ -312,11 +462,12 @@ server <- function(input, output){
   output$score <- renderDataTable({
     datatable(df.score(),
               colnames = c('Модель', 'RMSFE'),
-    options = list(
-      dom = 't',
+              rownames = FALSE,
+    options = list(dom = 'tip', 
       
-      order = list(list(2, 'asc'))
-    ))
+      order = list(list(1, 'asc'))
+    )) %>%
+      formatRound(columns=c(2), digits=3)
   })
   
   
@@ -363,12 +514,14 @@ for(modeli in out_true$model %>% unique){
     filter(model == modeli,
            startdt == max(startdt),
            lag != 0,
-           h != 0) %>%
+           h != 0,
+           date > min(enddt)) %>%
+    mutate(enddt.factor = as.factor(enddt)) %>%
     ggplot()+
-    geom_line(aes(x = date, y = pred), color = 'red')+
+    geom_line(aes(x = date, y = pred, group = enddt.factor), color = 'darkgreen', alpha = 0.2)+
     geom_line(aes(x = date, y = true))+
-    geom_vline(aes(xintercept  = min(enddt)),
-               color = "red",linetype="dashed")+
+    # geom_vline(aes(xintercept  = min(enddt)),
+    #            color = "red",linetype="dashed")+
     labs(title = modeli) +
     facet_grid(vars(lag), vars(h))
   png(paste0('plot/facets/',modeli,".png"), width = 1000, height = 700)
@@ -413,26 +566,43 @@ out_true %>%
 
 # график, на котром показано, как меняется rmse при изменении границ тренировочной выборки
 # rmse в 1997-01-01 (первая дата) для соответствующего лага и соответствующего горизонта прогнозирования = 1
+
+# меняем startdt
+
 scoredf %>%
-  filter(model != 'rw') %>%
+  right_join(optlag, by = c('model', 'startdt', 'enddt', 'h', 'lag')) %>%
+  filter(type == 'test', enddt == min(enddt)) %>%
+  filter(model != 'Random Walk') %>%
   group_by(model, lag, h) %>%
-  arrange(startdt) %>%
-  mutate(rmse = rmse/first(rmse)) %>%
+  arrange(enddt) %>%
+  #mutate(rmse = rmse/first(rmse)) %>%
   ggplot() +
   geom_line(aes(x = startdt, y = rmse, linetype = model, color=model), size = 0.703)+
-  facet_grid( vars(h),vars(lag))
+  facet_wrap( vars(h), ncol = 2) +
+  labs(x = 'Дата прогноза', y = 'RMSFE относительно RW') +
+  guides(colour = guide_legend("Модель"),
+         linetype = guide_legend("Модель"))
+theme()
+
+
+# меняем enddt
+scoredf %>%
+  right_join(optlag, by = c('model', 'startdt', 'enddt', 'h', 'lag')) %>%
+  filter(type == 'test', startdt == max(startdt)) %>%
+  filter(model != 'Random Walk') %>%
+  group_by(model, lag, h) %>%
+  arrange(startdt) %>%
+  #mutate(rmse = rmse/first(rmse)) %>%
+  ggplot() +
+  geom_line(aes(x = enddt, y = rmse, linetype = model, color=model), size = 0.703)+
+  facet_wrap( vars(h), ncol = 2) +
+  labs(x = 'Дата прогноза', y = 'RMSFE относительно RW') +
+  guides(colour = guide_legend("Модель"),
+         linetype = guide_legend("Модель"))
+  theme()
 # по графику видно, что модели LASSO и ARIMA очень чувствительна к уменьшению размеров выборки
 # с уменьешением размеров их качество падает
 # Осталные модели в среднем либо не ухудушают свое качество, либо значительно улучшают (ss)
-scoredf %>%
-  filter(model != 'rw') %>%
-  group_by(model, lag, h) %>%
-  arrange(startdt) %>%
-  mutate(rmse = rmse/first(rmse)) %>%
-  ggplot() +
-  geom_line(aes(x = startdt, y = rmse, linetype = model, color=model), size = 0.703)+
-  facet_grid( vars(h),vars(lag))
-
 
 # вычислим значимость коэффициентов в модели spike and slab ----
 rm(list=ls())
@@ -509,3 +679,6 @@ scoredf_raw %>%
   ggplot(aes(enddt, rmse, color = model))+
     geom_line()+
   facet_wrap(vars(h))
+
+
+  
