f0a983588de490a13435efd8fab9ecf71138d24c
Mikhail Gareev <grvmkhl@gmail.com>
2019-09-05 19:55:49 +0300

    Add all dates to jobs

--- job3.R
+++ job3.R
@@ -1,8 +1,8 @@
 source('lib.R')
 source('fun.R')
 
-out7 <- expand.grid(startdt = as.Date('2000-01-01'),
-                         enddt = seq(as.Date('2011-10-01'), as.Date('2015-07-01'), by = 'quarter'),
+out7 <- expand.grid(startdt = seq(as.Date('1997-01-01'), as.Date('2000-01-01'), by = 'quarter'),
+                         enddt = seq(as.Date('2011-10-01'), as.Date('2015-01-01'), by = 'quarter'),
                     lag = c(0L:4L),
                     h=c(0L:4L), 
                     model = c(
--- job4.R
+++ job4.R
@@ -2,16 +2,11 @@ source('lib.R')
 source('fun.R')
 
 out4 <- expand.grid(startdt = seq(as.Date('1997-01-01'), as.Date('2000-01-01'), by = 'quarter'),
-                    enddt = seq(as.Date('2011-10-01'), as.Date('2015-07-01'), by = 'quarter'),
+                    enddt = seq(as.Date('2011-10-01'), as.Date('2015-01-01'), by = 'quarter'),
                     lag = c(0L:4L),
                     h=c(0L:4L), 
                     model = c('arima', 'rw')
-) %>%
-  filter(startdt == as.Date('2000-01-01') &
-           enddt >= as.Date('2011-10-01') |
-           startdt < as.Date('2000-01-01') &
-           enddt == as.Date('2011-10-01') 
-           ) %>%
+)  %>%
   split(seq(1:nrow(.))) %>%
   map(function(x){
     train.model(startdt=x$startdt,
