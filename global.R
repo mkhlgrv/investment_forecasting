8a56e86ba4d374274d3c2c09df0f2abe5053797c
Mikhail Gareev <grvmkhl@gmail.com>
2019-10-04 15:07:08 +0300

    Update global.R
    Add choises

--- global.R
+++ global.R
@@ -1,2 +1,8 @@
 source('lib.R')
-load('data/shinydata.RData')
+choises_q <- format(seq.Date(from = as.Date("2012-01-01"),
+                             to = as.Date("2019-01-01"),
+                             by = "quarter") %>%
+                      as.yearqtr,
+                    format = "%Y Q%q")
+
+load('shinydata.RData')
