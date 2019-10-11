79fefd865a432e32673f8b065c1c148fa077ad37
Mikhail Gareev <grvmkhl@gmail.com>
2019-10-11 13:30:42 +0300

    Update calibrating.R

--- calibrating.R
+++ calibrating.R
@@ -6,8 +6,10 @@ load('data/raw.Rdata')
 # 4 разность логарифма ВВП и ИПЦ, отношение номинальных инвестиций к номинальному ввп + лаги
 series <- c('investment', 'mkr_1d','mkr_7d','gov_6m','GKO', #'gov_1y',
             'reer', 'neer', 'oil', 'rts',
-            'CPI_Q_CHI', 'GDPEA_Q_DIRI', 'GDPEA_C_Q', 'INVFC_Q',
-            'EMPLDEC_Q')
+            'CPI_Q_CHI', 'GDPEA_Q_DIRI',
+            'GDPEA_C_Q', 'INVFC_Q',
+            'EMPLDEC_Q'
+            )
 df <- rawdata[,series]
 # отношение номинальных инвестиций к номинальному ВВП
 df$invest2gdp <- df$INVFC_Q/df$GDPEA_C_Q
@@ -29,7 +31,9 @@ for(i in c('reer','neer','oil','rts')){
 # нестационарные ряды с сезонностью (4-ая разность) ----
 for(i in c('investment', 'CPI_Q_CHI',
            'invest2gdp',
-           'GDPEA_Q_DIRI')){
+           'GDPEA_Q_DIRI',
+           
+           'EMPLDEC_Q')){
   df[,i] %<>% diff.xts(lag = 4, log=TRUE)
 }
 
