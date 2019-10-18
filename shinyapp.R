b12b02cd20b108b1f338dd75bf35f51b35491f98
Mikhail Gareev <grvmkhl@gmail.com>
2019-10-18 18:34:35 +0300

    Update shinyapp.R

--- shinyapp.R
+++ shinyapp.R
@@ -652,18 +652,8 @@ ssdatatest %>%
   ##### PC
 load('data/stationary_data.RData')
 
-datatopc <- df %>% as.data.frame() %>% na.omit()
+load('data/raw.RData')
 
-pc <- datatopc%>% as.matrix() %>% na.omit%>% prcomp()
-pc %>% summary
-ggplot(aes(pc$x[,2],datatopc$investment),data=NULL)+
-  geom_point(aes(color=datatopc %>% rownames() %>% as.yearqtr %>% year %>% as.factor()))+
-  geom_smooth(method='lm')
+load('jobs/out_lasso.RData')
 
-model <- cv.spikeslab(x = dplyr::mutate(df %>% as.data.frame, y=lag.xts(investment,-1)) %>%
-               na.omit %>%
-               select(-y),
-             y= dplyr::mutate(df%>% as.data.frame, y=lag.xts(investment,-1)) %>%
-               na.omit %>%
-               pull(y),
-             K = 10, verbose=FALSE)
+
