44c2f9c58ee2d6ff815fda13f3cb013623c00e03
Mikhail Gareev <grvmkhl@gmail.com>
2019-11-04 08:24:07 +0300

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -2,30 +2,14 @@
 rm(list = ls())
 source("lib.R")
 source("fun.R")
+load('data/stationary_data_ext.RData')
 
-
-load("rawdata.RData")
-load("out1.RData")
-load("out2.RData")
-load("out3.RData")
-load("out4.RData")
-load("out5.RData")
-load("out6.RData")
-load("out7.RData")
-load("out8.RData")
-load("out9.RData")
-load("out10.RData")
-load("out11.RData")
-save.image(file = "out.RData")
-load("out.RData")
-
-
-# investment level
+# investment level -----
 invest_plot <-
   ggplot(df %>% na.omit) +
   geom_line(aes(y = investment, x = time(df %>% na.omit)))+
   labs(title = "",
-      y = "валовое накопление основного капитала",
+      y = "Валовое накопление основного капитала",
        x = "Дата") +
   theme_bw()
 cairo_pdf("plot/invest_plot.pdf", width = 10, height = 5)
@@ -33,350 +17,269 @@ print(invest_plot)
 dev.off()
 
 
-
-
-##графики в уровнях
-outall <- list(out1, out2,
-     out3,
-     out4, out5, out6,
-     out7, out8, out9, out10,
-     out11) %>% map(function(x){
-       x %>% select(model,window,horizon,date,y.true,y.pred, y.true.ts, y.pred.ts) %>%
-         unique
-     }) %>% 
-  do.call.pipe(rbind )
-
-outall %<>% split(1:nrow(.)) %>%
-  map(function(x){
-    x$model <- as.character(x$model)
-    x$model <- switch(x$model[1],
-           acc = "Accelerator",
-           lasso = "LASSO",
-           "lasso_adaptive" = "Adaptive LASSO",
-           "post_lasso" = "Post LASSO",
-           lasso_pc = "LASSO+PC",
-           elnet = "Elastic Net",
-           ridge = "Ridge",
-           rf = "Random Forest",
-           boosting = "Boosting",
-           "ss" = "Spike and Slab",
-           VAR = "LASSO+VAR")
-    x
-  }) %>% do.call.pipe(rbind)
-
-
-
-
-
-
-
-
-## nonzero coefs ----
-# reglist[c(1,4,5)]
-
-nonzerotime <- do.call(rbind, list(out2, out3, out4, out5, out6)) %>%
-  split(1:nrow(.)) %>%
-  map(function(x){
-    x$model <- as.character(x$model)
-    x$model <- switch(x$model[1],
-                      acc = "Accelerator",
-                      lasso = "LASSO",
-                      "lasso_adaptive" = "Adaptive LASSO",
-                      "post_lasso" = "Post LASSO",
-                      lasso_pc = "LASSO+PC",
-                      elnet = "Elastic Net",
-                      ridge = "Ridge",
-                      rf = "Random Forest",
-                      boosting = "Boosting",
-                      "ss" = "Spike and Slab",
-                      VAR = "LASSO+VAR")
-    x
-  }) %>%
-  
-  do.call.pipe(rbind) %>%
-  filter(horizon == 1) %>%
-  group_by(model, horizon, date) %>%
-  summarise(nonzero = mean(nonzero)) %>% 
-  ungroup() %>%
-  ggplot()+
-  geom_line(aes(x = date, y = nonzero, colour = model), size = 0.8)+
+## all vars plot ----
+all_plot <-
+  df %>%
+  na.omit %>%
+  as.data.frame %>%
+  rownames_to_column('date') %>%
+  melt(id.vars = 'date') %>%
+  group_by(variable) %>%
+  mutate(value = scale(value),
+         date = as.Date(as.yearqtr(date))) %>%
+  ggplot() +
+  geom_line(aes(date, value, group=variable,
+                alpha = ifelse(variable %in% c('GDPEA_Q_DIRI' ,'investment'), 1, 0.3)), show.legend = F)+
   labs(title = "",
-       y = "Количество ненулевых коэффициентов",
-       x = "Дата") +
-  theme_bw()+
-  guides(colour = guide_legend(title = "Модель"))
-
-cairo_pdf("plot/nonzerotime.pdf", width = 10, height = 5)
-print(nonzerotime)
-dev.off()
-
-
-nonzeroerror <-
-  do.call(rbind, list(out2, out3, out4, out5, out6)) %>%
-  split(1:nrow(.)) %>%
-  map(function(x){
-    x$model <- as.character(x$model)
-    x$model <- switch(x$model[1],
-                      acc = "Accelerator",
-                      lasso = "LASSO",
-                      "lasso_adaptive" = "Adaptive LASSO",
-                      "post_lasso" = "Post LASSO",
-                      lasso_pc = "LASSO+PC",
-                      elnet = "Elastic Net",
-                      ridge = "Ridge",
-                      rf = "Random Forest",
-                      boosting = "Boosting",
-                      "ss" = "Spike and Slab",
-                      VAR = "LASSO+VAR")
-    x
-  }) %>%
-  do.call.pipe(rbind) %>%
-  mutate(error = (y.true - y.pred)^2) %>%
-  group_by(model, horizon, date) %>%
-  summarise(nonzero = mean(nonzero),
-            error = mean(error)) %>%
-    ungroup() %>%
-    mutate(horizon = as.numeric(horizon)) %>%
-  ggplot()+
-  geom_point(aes(y = error, x = nonzero, colour = model), size = 1.1, alpha = 1)+
-    geom_smooth(aes(y = error, x = nonzero,colour = model), se = FALSE, method = "lm")+
-  facet_wrap(vars(horizon), scales = "free")+
-  labs(title = "",x = "Количество ненулевых коэффициентов",
-       y = "Квадрат ошибки (логарифмическая шкала)") +
-  theme_bw()+
-  scale_y_continuous(trans='log10')+
-  guides(colour = guide_legend(title = "Модель"))
-
-cairo_pdf("plot/nonzeroerror.pdf", width = 14, height = 7)
-print(nonzeroerror)
+       y = "",
+       x = "") +
+  scale_x_date(limits = c('2001-01-01', '2019-01-01') %>% as.Date)+
+  scale_y_continuous(limits = c(-5, 3))+
+  theme_transparent()
+cairo_pdf("plot/allvars.pdf", width = 10, height = 5)
+print(all_plot)
 dev.off()
 
 
 
-nzmat1 <- do.call(rbind, list( #out3)) %>% #, out4, out5, 
-                             out6)) %>%
-  select(date, model, horizon, nzvars) %>%
-  arrange(date) %>%
-  unique
+##### rmsfe table -----
+load('data/stationary_data.RData')
 
-for(j in 2:ncol(df_long)){
-  nzmat1[j+3] <- 0
-  for(i in 1:nrow(nzmat1)){
-    if(grepl(paste0(" ",j-1, " "),nzmat1$nzvars[i])){
-      nzmat1[i,j+3] <- 1
-    } 
-  }
-}
+load('data/raw.RData')
 
-colnames(nzmat1)[-c(1:4)] <- colnames(df_long)[-1]
-nzlollipop1 <- nzmat1 %>%
-  group_by(model, horizon) %>%
-  select(-nzvars) %>% melt(id.vars = c("model", "horizon", "date"), ) %>%
-  group_by(model, horizon, variable) %>%
-  summarise(nonzero = sum(value)) %>%
+
+
+library(xtable)
+
+
+scoredf$model <- factor(scoredf$model,
+                        levels = c("Random Walk","AR","Adaptive LASSO",
+                                   "Elastic Net","LASSO","Post-LASSO",
+                                   "Random Forest","Ridge","Spike-and-Slab"))
+scoredf %>%
+  filter(type == 'test') %>%
+  filter(startdt == '1997-01-01') %>%
+  group_by(model, lag, h, startdt) %>%
+  summarise(rmse = mean(rmse)) %>%
   ungroup %>%
-  mutate(nonzero = nonzero/max(nonzero)) %>% 
-  inner_join(series_info %>% select(tsname, fullname), by = c("variable" ="tsname")) %>%
-  select(-model) %>%
-  filter(horizon == 1) %>%
-  arrange(nonzero) %>%
-  mutate(fullname = factor(fullname, fullname)) %>%
-  ggplot() +
-  geom_segment(aes(x = 0, y = fullname, xend = nonzero, yend = fullname), color = "red")+
-  labs(title = "",x = "Доля включений",
-       y = "") +
-  theme_bw()
-  
+  group_by(model, startdt, h) %>%
+  filter(rmse == min(rmse)) %>%
+  filter(lag == min(lag)) %>%
+  dcast(model ~ h) %>%
+  xtable %>%
+  print(include.rownames = FALSE)
 
-cairo_pdf("plot/nzlollipop1.pdf", width = 12, height = 7)
-print(nzlollipop1)
-dev.off()
-
-nzmat2 <- do.call(rbind, list( out3)) %>% #, out4, out5, 
-  #out6)) %>%
-  select(date, model, horizon, nzvars) %>%
-  arrange(date) %>%
-  unique
-
-for(j in 2:ncol(df_long)){
-  nzmat2[j+3] <- 0
-  for(i in 1:nrow(nzmat2)){
-    if(grepl(paste0(" ",j-1, " "),nzmat2$nzvars[i])){
-      nzmat2[i,j+3] <- 1
-    } 
-  }
-}
-
-colnames(nzmat2)[-c(1:4)] <- colnames(df_long)[-1]
-nzlollipop2 <- nzmat2 %>%
-  group_by(model, horizon) %>%
-  select(-nzvars) %>% melt(id.vars = c("model", "horizon", "date"), ) %>%
-  group_by(model, horizon, variable) %>%
-  summarise(nonzero = sum(value)) %>%
+scoredf %>%
+  filter(type == 'test') %>%
+  filter(startdt == '2001-01-01') %>%
+  group_by(model, lag, h, startdt) %>%
+  summarise(rmse = mean(rmse)) %>%
   ungroup %>%
-  mutate(nonzero = nonzero/max(nonzero)) %>% 
-  inner_join(series_info %>% select(tsname, fullname), by = c("variable" ="tsname")) %>%
-  select(-model) %>%
-  filter(horizon == 1) %>%
-  arrange(nonzero) %>%
-  mutate(fullname = factor(fullname, fullname)) %>%
-  ggplot() +
-  geom_segment(aes(x = 0, y = fullname, xend = nonzero, yend = fullname), color = "red")+
-  labs(title = "",x = "Доля включений",
-       y = "") +
-  theme_bw()
-
-
-cairo_pdf("plot/nzlollipop2.pdf", width = 12, height = 7)
-print(nzlollipop2)
-dev.off()
-
-
-#### таблица -----
-
-tab1 <- outall %>%
-  filter(model!="Accelerator") %>%
-  get.score() %>%
-  ungroup %>%
-  split(1:nrow(.)) %>%
-  map_dfr(function(x){
-    x$rmse <-x$rmse/ benchmark.score %>% filter(horizon == x$horizon) %>% pull(rmse)
-    x
-  }) %>% 
-  select(model, horizon, rmse) %>%
-  rename(Модель = model ) %>%
-  filter(horizon<=6) %>%
-  dcast(Модель~horizon) %>%
-  xtable%>%
-    print(include.rownames = FALSE)
-
-tab2 <- outall %>%
-  filter(model!="Accelerator") %>%
-  get.score() %>%
-  ungroup %>%
-  split(1:nrow(.)) %>%
-  map_dfr(function(x){
-    x$rmse <-x$rmse/ benchmark.score %>% filter(horizon == x$horizon) %>% pull(rmse)
-    x
-  }) %>% 
-  select(model, horizon, rmse) %>%
-  rename(Модель = model ) %>%
-  filter(horizon>6) %>%
-  dcast(Модель~horizon) %>%
+  group_by(model, startdt, h) %>%
+  filter(rmse == min(rmse)) %>%
+  filter(lag == min(lag)) %>%
+  dcast(model ~ h) %>%
   xtable %>%
   print(include.rownames = FALSE)
 
 
 
-error1 <- outall %>%
-  filter(model!="Accelerator")%>%
-  filter(model %in% c("LASSO", "Ridge", "Elastic Net")) %>%
-  mutate(error = y.true - y.pred) %>%
-  ggplot() +
-  geom_density(aes(x = error, color = model)) +
-  facet_wrap(vars(horizon)) + 
-  labs(title = "",x = "Ошибка",
-       y = "") +
-  theme_bw()+
-  guides(colour = guide_legend(title = "Модель"))
-
-cairo_pdf("plot/error1.pdf", width = 12, height = 7)
-print(error1)
-dev.off()
-
-
-error2 <- outall %>%
-  filter(model!="Accelerator")%>%
-  filter(model %in% c("LASSO","LASSO+PC", "Post LASSO", "Adaptive LASSO","LASSO+VAR")) %>%
-  mutate(error = y.true - y.pred) %>%
-  ggplot() +
-  geom_density(aes(x = error, color = model)) +
-  facet_wrap(vars(horizon)) + 
-  labs(title = "",x = "Ошибка",
-       y = "") +
-  theme_bw()+
-  guides(colour = guide_legend(title = "Модель"))
-
-cairo_pdf("plot/error2.pdf", width = 12, height = 7)
-print(error2)
-dev.off()
-
-error3 <- outall %>%
-  filter(model!="Accelerator")%>%
-  filter(model %in% c("Random Forest","Spike and Slab", "Boosting")) %>%
-  mutate(error = y.true - y.pred) %>%
-  ggplot() +
-  geom_density(aes(x = error, color = model)) +
-  facet_wrap(vars(horizon)) + 
-  labs(title = "",x = "Ошибка",
-       y = "") +
-  theme_bw()+
-  guides(colour = guide_legend(title = "Модель"))
-
-cairo_pdf("plot/error3.pdf", width = 12, height = 7)
-print(error3)
-dev.off()
-
-ertab1 <- check.normal(outall %>% filter(model != "Accelerator")) %>%
-  select(model, horizon, pv) %>%
-  rename(Модель = model ) %>%
-  filter(horizon<=6) %>%
-  dcast(Модель~horizon) %>%
-  xtable%>%
+scoredf %>%
+  filter(type == 'test') %>%
+  filter(startdt == '1997-01-01') %>%
+  group_by(model, lag, h, startdt) %>%
+  summarise(rmse = mean(rmse)) %>%
+  ungroup %>%
+  group_by(model, startdt, h) %>%
+  filter(rmse == min(rmse)) %>%
+  filter(lag == min(lag)) %>%
+  dcast(model ~ h, value.var ='lag') %>%
+  xtable %>%
   print(include.rownames = FALSE)
 
-ertab2 <- check.normal(outall %>% filter(model != "Accelerator")) %>%
-  select(model, horizon, pv) %>%
-  rename(Модель = model ) %>%
-  filter(horizon>=7) %>%
-  dcast(Модель~horizon) %>%
-  xtable%>%
+scoredf %>%
+  filter(type == 'test') %>%
+  filter(startdt == '2001-01-01') %>%
+  group_by(model, lag, h, startdt) %>%
+  summarise(rmse = mean(rmse)) %>%
+  ungroup %>%
+  group_by(model, startdt, h) %>%
+  filter(rmse == min(rmse)) %>%
+  filter(lag == min(lag)) %>%
+  dcast(model ~ h, value.var ='lag') %>%
+  xtable %>%
+  print(include.rownames = FALSE)
+
+#### dm test ----
+
+
+dmdf <- get.dm(out_true %>% na.omit)
+
+dmsd <- dmdf %>%
+  filter(model != 'Random Walk') %>%
+  mutate(change = ifelse(pvalue > 0.01,
+                         '0',
+                         ifelse(stat < 0,
+                                '+',
+                                '-')
+  )) %>%
+  ggplot(aes( model,interaction(h, lag))) +
+  geom_raster(aes(fill = change))
+
+cairo_pdf('plot/dmsd.pdf')
+print(dmsd)
+dev.off()
+
+
+dmtest97 <- get.dm.lag(out_true %>% na.omit) %>%
+  filter(!model %in% c('Random Walk', 'AR'),
+         startdt == '1997-01-01') %>%
+  # mutate(change = ifelse(pvalue > 0.01,
+  #                        '0',
+  #                        ifelse(stat < 0,
+  #                               '+',
+  #                               '-')
+  # )) %>%
+  mutate(less = ifelse(pvalue >0.05 , FALSE, TRUE)) %>%
+  ggplot(aes(lag, interaction(model))) +
+  geom_raster(aes(fill = less))+
+  facet_wrap(vars(h))
+
+cairo_pdf('plot/dmtest97.pdf')
+print(dmtest97)
+dev.off()
+
+
+dmtest01 <- get.dm.lag(out_true %>% na.omit) %>%
+  filter(!model %in% c('Random Walk', 'AR'),
+         startdt == '2001-01-01') %>%
+  # mutate(change = ifelse(pvalue > 0.01,
+  #                        '0',
+  #                        ifelse(stat < 0,
+  #                               '+',
+  #                               '-')
+  # )) %>%
+  mutate(less = ifelse(pvalue >0.05 , FALSE, TRUE)) %>%
+  ggplot(aes(lag, interaction(model))) +
+  geom_raster(aes(fill = less))+
+  facet_wrap(vars(h))
+
+cairo_pdf('plot/dmtest01.pdf')
+print(dmtest01)
+dev.off()
+
+
+
+
+# вычисление предикторов adalasso ----
+source('fun.R')
+source('lib.r')
+
+load('out/full/out_lasso.RData')
+load('out/full/out_adalasso.RData')
+
+
+lasso_beta <- ovut_lasso %>%
+  plyr::compact()%>%
+  map_dfr(
+    function(x){
+      betaval = x$model_fit$beta 
+      x$model_fit$beta
+      data.frame(model = x$model,
+                 lag = x$lag,
+                 h = x$h, 
+                 startdt=x$startdt,
+                 enddt = x$enddt,
+                 predictor = betaval%>% rownames(),
+                 beta = betaval%>% as.numeric
+      )
+      
+    }
+  )
+
+# количество переменных
+
+adalasso_beta <- out_adalasso %>%
+  plyr::compact()%>%
+  map_dfr(
+  function(x){
+    betaval = x$model_fit$beta 
+    x$model_fit$beta
+    data.frame(model = x$model,
+               lag = x$lag,
+               h = x$h, 
+               startdt=x$startdt,
+               enddt = x$enddt,
+               predictor = betaval%>% rownames(),
+               beta = betaval%>% as.numeric
+    )
+    
+  }
+)
+
+# количество переменных
+ada_nonzero <-  adalasso_beta %>%
+  filter(lag == 4) %>%
+  mutate(startdt = factor(startdt, c('2001-01-01','1997-01-01'))) %>%
+  group_by(lag, h, startdt, enddt) %>%
+  summarise(nz = sum(beta != 0)) %>%
+  ggplot(aes(enddt, nz, linetype = startdt))+
+  geom_line()+
+  labs(title = "",
+       y = "Количество переменных",
+       x = "Дата",
+       color = '')+
+  facet_wrap(vars(h))+
+  theme_bw()
+
+
+
+cairo_pdf('plot/ada_nonzero.pdf')
+print(ada_nonzero)
+dev.off()
+
+
+adalasso_beta %>% group_by(predictor, lag, h, startdt) %>%
+  filter(lag==0,startdt== '2001-01-01') %>%
+  filter(h<2) %>%
+  group_by(predictor, h) %>%
+  summarise(beta = mean(beta)) %>%
+  ungroup %>%
+  group_by(h) %>%
+  arrange(desc(abs(beta))) %>%
+  mutate(rn = row_number(),
+         pred_beta = paste0(predictor, round(beta,3))) %>%
+  filter(rn<=40) %>%
+  ungroup %>%
+  dcast(rn~h, value.var = 'pred_beta') %>%
+  xtable %>%
   print(include.rownames = FALSE)
 
 
-df_short %>%
-  as.data.frame() %>%
-  melt %>%
-inner_join(series_info %>% select(tsname, fullname), by = c("variable" ="tsname")) %>%
-  select(fullname) %>% unique %>%
-  xtable%>%
+adalasso_beta %>% group_by(predictor, lag, h, startdt) %>%
+  filter(lag==4,startdt== '2001-01-01') %>%
+  filter(h>4) %>%
+  group_by(predictor, h) %>%
+  summarise(beta = mean(beta)) %>%
+  ungroup %>%
+  group_by(h) %>%
+  arrange(desc(abs(beta))) %>%
+  mutate(rn = row_number(),
+         pred_beta = paste0(predictor, round(beta,3))) %>%
+  filter(rn<=10) %>%
+  ungroup %>%
+  dcast(rn~h, value.var = 'pred_beta') %>%
+  xtable %>%
   print(include.rownames = FALSE)
   
-
-
-
-tab_app1 <- outall %>%
-  filter(model!="Accelerator") %>%
-  get.score() %>%
-  ungroup %>%
-  split(1:nrow(.)) %>%
-  map_dfr(function(x){
-    x$mae <-x$mae/ benchmark.score %>% filter(horizon == x$horizon) %>% pull(mae)
-    x
-  }) %>%
-  select(model, horizon, mae) %>%
-  rename(Модель = model ) %>%
-  filter(horizon<=6) %>%
-  dcast(Модель~horizon) %>%
-  xtable%>%
-  print(include.rownames = FALSE)
-
-tab_app2 <- outall %>%
-  filter(model!="Accelerator") %>%
-  get.score() %>%
-  ungroup %>%
-  split(1:nrow(.)) %>%
-  map_dfr(function(x){
-    x$mae <-x$mae/ benchmark.score %>% filter(horizon == x$horizon) %>% pull(mae)
-    x
-  }) %>%
-  select(model, horizon, mae) %>%
-  rename(Модель = model ) %>%
-  filter(horizon>6) %>%
-  dcast(Модель~horizon) %>%
-  xtable%>%
-  print(include.rownames = FALSE)
-
-
-
+  mutate(predictor = factor(predictor, predictor %>% unique)) %>%
   
+  ggplot()+
+  #geom_line(aes(h, includes, color=predictor))
+  geom_segment(aes(x = 0, xend = includes, y = predictor, yend = predictor, color=as.factor(sign)))+
+  facet_wrap(vars(h))
+
+
+
+
+
